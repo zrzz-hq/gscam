020471035c20ecc7cdefc8671ffb06a4023bbc1b
nvidia <zhangrenzhongzheng@outlook.com>
2026-02-03 20:40:16 -0500

    fix bugs in gscam.cpp

--- src/gscam.cpp
+++ src/gscam.cpp
@@ -390,9 +390,10 @@ namespace gscam {
       if(buf) {
 #if (GST_VERSION_MAJOR == 1)
         gst_memory_unmap(memory, &info);
-        gst_memory_unref(memory);
-#endif
+        gst_sample_unref(sample);
+#else
         gst_buffer_unref(buf);
+#endif
       }
 
       ros::spinOnce();
